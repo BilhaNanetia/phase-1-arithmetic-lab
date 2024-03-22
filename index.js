cf0f7e5e0979282b3838f6c804f79a3028fa2586
Bilha <bilha.leposo@student.moringaschool.com>
2024-03-22 16:55:46 +0300

    complete arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
+ //Define two variables
+ let num1 = 31;
+ let num2 = 2;
+ let multiply = num1 * num2
+ console.log(multiply); //This will output 62
+
+ //Using Math.floor() 
+ let random = Math.floor (Math.random() * 10) + 1;
+ console.log(random); //This will output a random integer greater than 0
+
+ 
+ //Define two variables
+ let num4 = 8; //let num4 be a multiple of 4
+ let num3 = num4 + 4;
+ let mod = num3 % num4;
+ console.log(mod); // This will output remainder of 4
+
+
+ //Using Math.max
+ let numbers = [15, 16, 17, 18, 19, 20]; //Set of numbers
+ let max = Math.max(15, 16, 17, 18, 19, 20);
+ console.log(max); //This will output 20
 
 // Write your code here
